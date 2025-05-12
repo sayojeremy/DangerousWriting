e02a08c3ac33217b9faf56384f8bba73f1a55897
Sayo <sayomilembo@gmail.com>
2025-05-12 08:46:54 +0300

    Logging of times between key presses

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ import tkinter as tk
 from datetime import datetime
 import random
 
+# === Global Variables ===
+previous_keystroke_time = None
+# completion_time = None
+# duration = None
+
+# # === Reset Function ===
+# def reset():
+#     global first_keystroke_time, completion_time, duration
+#     previous_keystroke_time = None
+#     text.delete("1.0", tk.END)
+#     label_result.config(text="Start typing again. End with '/' to finish.")
+
+# === First Key Detection ===
+def on_keypress(event):
+    global previous_keystroke_time
+    current_keystroke_time= datetime.now()
+    if previous_keystroke_time is not None:
+        gap= (current_keystroke_time-previous_keystroke_time).total_seconds()
+        print(f"The gap between two characters was {gap}")
+
+    else:
+        print(f"First key '{event.char}' pressed at {current_keystroke_time}")
+
+    previous_keystroke_time = current_keystroke_time
+
+
+
 # === UI Setup ===
 root = tk.Tk()
 root.title("Dangerous typing")
@@ -18,7 +45,17 @@ header = tk.Label(root, text="Dangerous typing ", font=("Helvetica", 14, "bold")
                   bg="#f7f7f7", fg="#2c3e50")
 header.pack(pady=10)
 
+# === Text Input ===
+text = tk.Text(root, height=20, width=100, font=font_text, bd=2, relief="groove")
+text.pack(pady=5)
+text.bind("<Key>", on_keypress)
 
+
+
+# === Result Label ===
+label_result = tk.Label(root, text="", wraplength=480, justify="left",
+                        font=font_result, bg="#f7f7f7", fg="#444")
+label_result.pack(pady=10)
 
 
 
